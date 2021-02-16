f95b45f58c320b306bae1ba890594d6685f8233a
meco00 <mertkadir19@abv.bg>
2021-02-16 15:49:28 +0200

    Adding ado.net lab

---
+++ ADO.NETLAB/ADO.netPRACTICE/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.IO;
+
+namespace ADO.netPRACTICE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var practiceWith13ExamFebOnMSSQL = new PracticeExamWithADO("zaza");
+
+            practiceWith13ExamFebOnMSSQL.Run();
+
+
+            var practiceInjection = new SQLInjection();
+            practiceInjection.Run();
+
+
+         
+
+        }
+
+       
+        }
+    }
+
---
+++ ADO.NETLAB/ADO.netPRACTICE/SQLInjection.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ADO.netPRACTICE
+{
+    public class SQLInjection
+    {
+     
+
+        public void Run()
+        {
+            SqlInjectionPrevent();
+
+        }
+
+        private static void SqlInjectionPrevent()
+        {
+            var dbconnection = new SqlConnection("Server=.;Database=Demo;Integrated Security=true");
+            using (dbconnection)
+            {
+                dbconnection.Open();
+
+                Console.WriteLine("Please Enter Username");
+                var inputUsername = Console.ReadLine();
+
+                Console.WriteLine("Please Enter Password");
+                var inputPassword = Console.ReadLine();
+
+
+
+                var command = new SqlCommand(
+                   @$" SELECT COUNT(*) FROM
+                      Customers
+                      WHERE FirstName=@FirstName AND PaymentNumber=@PaymentNumber; ", dbconnection);
+
+
+                //With this method Sql Injection Is  prevented 
+
+                command.Parameters.AddWithValue("@FirstName", inputUsername);
+                command.Parameters.AddWithValue("@PaymentNumber", inputPassword);
+
+
+                int IsValid = (int)command.ExecuteScalar();
+
+                if (IsValid > 0)
+                {
+                    Console.WriteLine("Access granted");
+
+                }
+                else
+                {
+                    Console.WriteLine("Access denied");
+                }
+
+
+
+
+
+
+
+
+
+            }
+        }
+    }
+}
+
